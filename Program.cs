c8c98655f761339dc80333a1e73b58486e67b85d
jprochao <jprochao@gmail.com>
2025-07-28 22:01:45 -0300

    Desafio POO completado com as solicitações TODO.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone iphone = new Iphone("9988-9999", "Iphone14", "35/990233", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facetime");
+
+Smartphone nokia = new Nokia("9987-8998", "Nokia Xpiria", "36/990344", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("YoutubeMusic");
